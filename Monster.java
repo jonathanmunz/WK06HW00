31e80df5caa2a92cee85f2ef245307d2752de799
Jonathan <jmunz@csumb.edu>
2019-03-01 11:51:16 -0800

    Finished part 1.

---
+++ Monster.java
@@ -0,0 +1,63 @@
+import java.util.HashMap;
+
+public abstract class Monster {
+
+    private Integer hp;
+    private Integer xp;
+    private Integer maxHP;
+    private HashMap<String, Integer> items;
+
+    public Monster(Integer maxHP, Integer xp, HashMap<String, Integer> items){
+        this.maxHP = maxHP;
+        hp = this.maxHP;
+        this.xp = xp;
+        this.items = items;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+    public Integer getXp() {
+        return xp;
+    }
+
+    public void setXp(Integer xp) {
+        this.xp = xp;
+    }
+
+    public Integer getMaxHP() {
+        return maxHP;
+    }
+
+    public void setMaxHP(Integer maxHP) {
+        this.maxHP = maxHP;
+    }
+
+    public HashMap<String, Integer> getItems() {
+        return items;
+    }
+
+    public void setItems(HashMap<String, Integer> items) {
+        this.items = items;
+    }
+
+
+    public boolean equals(Object name) {
+        return true;
+    }
+
+    public int hashCode() {
+        return 1;
+    }
+
+    @Override
+    public String toString () {
+        return "hp= " + this.hp + "/" + maxHP;
+    }
+
+}
